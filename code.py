6056b69519a1d050eedefaebec8db87b521c8429
John McAleely <john@mcaleely.com>
2024-07-16 22:12:20 +0100

    move LED cube to separate file

--- code.py
+++ code.py
@@ -10,11 +10,11 @@
 # Button B seeds the two Life arrays with one random live cell
 
 import picoed
-import life
 import time
 import random
-import neopixel
-import board
+
+import life
+import cubeled
 
 
 def make_plane_world():
@@ -35,40 +35,18 @@ def display_plane(world):
                 pixel_bright = liveLED
             picoed.display.pixel(i, j, pixel_bright)
             
-cube_side = 4
 
 def make_cube_world():
-    return life.make_world(cube_side, cube_side**2)
+    return life.make_world(cubeled.cube_side, cubeled.cube_side**2)
 
 # The Life array for the CUBE:BIT Led Cube.
 cube_world = make_cube_world()
 
-# Pico:ed is connected to the 'neopixel' string via Micro:Bit
-# pin 0. CUBE:BIT has GRB Leds.
-cube_display_pixels = neopixel.NeoPixel(board.P0,
-                                        cube_side**3,
-                                        # avoid brightness over 40% to stay within USB
-                                        # power budget.
-                                        brightness=0.1,
-                                        auto_write=False,
-                                        pixel_order = neopixel.GRB)
-
-# the LEDs are arranged serially, snaking around the cube.
-cube_index = [ 0, 1, 2, 3, 7, 6, 5, 4, 8, 9,10,11,15,14,13,12,
-              28,27,20,19,29,26,21,18,30,25,22,17,31,24,23,16,
-              32,33,34,35,39,38,37,36,40,41,42,43,47,46,45,44,
-              60,59,52,51,61,58,53,50,62,57,54,49,63,56,55,48
-              ]
-
-def set_cube_pixel(x, y, z, colour):
-    global cube_display_pixels
-    cube_display_pixels[cube_index[z*16+y*4+x]] = colour
-
 def get_cube_cell(world, x, y, z):
-    return life.get_cell(world, x, y+z*cube_side)
+    return life.get_cell(world, x, y+z*cubeled.cube_side)
 
 def set_cube_cell(world, x, y, z, state):
-    life.set_cell(world, x, y+z*cube_side, state)
+    life.set_cell(world, x, y+z*cubeled.cube_side, state)
 
 
 def display_cube(world):
@@ -76,14 +54,14 @@ def display_cube(world):
     live = (128,0,0)
     dead = (16,0,0)
 
-    for k in range(cube_side):
-        for j in range(cube_side):
-            for i in range(cube_side):
+    for k in range(cubeled.cube_side):
+        for j in range(cubeled.cube_side):
+            for i in range(cubeled.cube_side):
                 pixel_colour = dead
                 if get_cube_cell(world, i, j, k) == life.live:
                     pixel_colour = live
-                set_cube_pixel(i,j,k, pixel_colour)
-    cube_display_pixels.show()
+                cubeled.set_pixel(i,j,k, pixel_colour)
+    cubeled.cube_display_pixels.show()
 
 
 def set_random_cell():
@@ -91,9 +69,9 @@ def set_random_cell():
     j = random.randrange(life.row_count(plane_world))
     life.set_cell(plane_world, i, j, life.live)
         
-    i = random.randrange(cube_side)
-    j = random.randrange(cube_side)
-    k = random.randrange(cube_side)
+    i = random.randrange(cubeled.cube_side)
+    j = random.randrange(cubeled.cube_side)
+    k = random.randrange(cubeled.cube_side)
     set_cube_cell(cube_world, i, j, k, life.live)
 
 
