b0624efab6e6f248e33e622884adb3f903264029
John McAleely <john@mcaleely.com>
2025-03-31 12:51:33 +0100

    Move to GPIO18, so GPIO on pico matches 40-pin Pi header

--- C/ws2812.c
+++ C/ws2812.c
@@ -30,12 +30,8 @@
 // limit brightness so that 2.5A USB power supply is sufficient.
 #define PIXEL_MAX 128
 
-#ifdef PICO_DEFAULT_WS2812_PIN
-#define WS2812_PIN PICO_DEFAULT_WS2812_PIN
-#else
-// default to pin 2 if the board doesn't have a default WS2812 pin defined
-#define WS2812_PIN 2
-#endif
+// Match the GPIO from Raspberry Pi 40-Pin on 4Tronix Cube:BIT (GPIO18)
+#define WS2812_GPIO 18
 
 // Check the pin is compatible with the platform
 #if WS2812_PIN >= NUM_BANK0_GPIOS
@@ -134,7 +130,7 @@ const struct {
 int main() {
     //set_sys_clock_48();
     stdio_init_all();
-    printf("WS2812 Smoke Test, using pin %d\n", WS2812_PIN);
+    printf("WS2812 Smoke Test, using pin %d\n", WS2812_GPIO);
 
     // todo get free sm
     PIO pio;
@@ -144,10 +140,10 @@ int main() {
     // This will find a free pio and state machine for our program and load it for us
     // We use pio_claim_free_sm_and_add_program_for_gpio_range (for_gpio_range variant)
     // so we will get a PIO instance suitable for addressing gpios >= 32 if needed and supported by the hardware
-    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&ws2812_program, &pio, &sm, &offset, WS2812_PIN, 1, true);
+    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&ws2812_program, &pio, &sm, &offset, WS2812_GPIO, 1, true);
     hard_assert(success);
 
-    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+    ws2812_program_init(pio, sm, offset, WS2812_GPIO, 800000, IS_RGBW);
 
     int t = 0;
     while (1) {
